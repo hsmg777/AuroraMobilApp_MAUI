79cbdea8059c965623ea83f953b6c1e501951260
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-11-20 16:47:20 -0500

    Models{ Creacion de Models Usuario } Views { labels y entrys para registrar user }

---
+++ Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraApp_MAUI.Models
+{
+    class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        [DisplayName("Nombre de Usuario")]
+        public string NomUsuario {  get; set; }
+
+        [StringLength(10)]
+        public string Celular { get; set; }
+
+        public string Contraseña { get; set; }
+    }
+}
