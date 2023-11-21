91f174613eec057949adee8ab7266af189504fb3
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-11-21 10:22:01 -0500

    metodos para incluir la BD

---
+++ Data/SQLiteData.cs
@@ -0,0 +1,21 @@
+﻿using AuroraApp_MAUI.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuroraApp_MAUI.Data
+{
+    public class SQLiteData
+    {
+        readonly SQLiteAsyncConnection _conexionBD;
+        //public UsuarioData UsuarioDataTable {  get; set; }
+        public SQLiteData(string path) {
+            _conexionBD = new SQLiteAsyncConnection(path);
+            _conexionBD.CreateTableAsync<Usuario>().Wait();
+
+        }    
+    }
+}
