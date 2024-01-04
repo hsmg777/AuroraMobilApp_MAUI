6dce37dd6625e788fcef15d83b35cfcc971f3d66
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2024-01-04 09:24:40 -0500

    YA SIRVIO TODO

--- ViewModels/mostrarReservaViewModel.cs
+++ ViewModels/mostrarReservaViewModel.cs
@@ -13,22 +13,7 @@ namespace AuroraApp_MAUI.ViewModels
             // Agregar dos reservas predeterminadas
             ReservasList = new List<Reservas>
             {
-                new Reservas
-                {
-                    nombre = "Jorge Moncayo",
-                    numPersonas = "5",
-                    telefono = "0997589387",
-                    fecha = DateTime.Now.AddDays(1),
-                    horaLlegada = new TimeSpan(18, 0, 0) // 6:00 PM
-                },
-                new Reservas
-                {
-                    nombre = "Hayland Montalvo 2",
-                    numPersonas = "3",
-                    telefono = "0999567465",
-                    fecha = DateTime.Now.AddDays(2),
-                    horaLlegada = new TimeSpan(20, 0, 0) // 8:00 PM
-                }
+
             };
             ReservasList.AddRange(App.reservaRepo.GetAll());
         }
