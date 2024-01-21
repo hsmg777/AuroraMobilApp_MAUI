8a584ead1ca570b752514ef5fd6685872487d12f
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2024-01-21 15:28:52 -0500

    Consumo de API para ver fiestas disponibles mediante boton ver fiestas

--- Views/MenuPrinc.xaml.cs
+++ Views/MenuPrinc.xaml.cs
@@ -1,19 +1,22 @@
+using AuroraApp_MAUI.Models;
 using AuroraApp_MAUI.ViewModels;
 using Microsoft.Maui;
+using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
+
 
 
 namespace AuroraApp_MAUI.Views
 {
     public partial class MenuPrinc : ContentPage
     {
-        private mostrarReservaViewModel reservationListViewModel;
+       
        
         public MenuPrinc()
         {
             InitializeComponent();
-            reservationListViewModel = new mostrarReservaViewModel();
+           
         }
 
        
@@ -28,9 +31,52 @@ namespace AuroraApp_MAUI.Views
             Shell.Current.GoToAsync(nameof(mostrarReservas));
         }
 
-        private void Actualizar_Clicked(object sender, EventArgs e)
+        private async void Actualizar_Clicked(object sender, EventArgs e)
         {
-            Shell.Current.GoToAsync(nameof(MenuReservas));
+            try
+            {
+                HttpClient client = new HttpClient();
+                string url = "https://localhost:7051/api/Fiestas";
+                var response = await client.GetStringAsync(url);
+
+                var fiestas = JsonConvert.DeserializeObject<List<Party>>(response);
+
+                var ultimasFiestas = fiestas.OrderByDescending(f => f.idFiesta).Take(3).ToList();
+
+                for (int i = 0; i < ultimasFiestas.Count; i++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            Fiesta1.Text = ultimasFiestas[i].nombreFiesta;
+                           
+                            imagen1.Source = ultimasFiestas[i].imagen;
+                            break;
+                        case 1:
+                            Fiesta2.Text = ultimasFiestas[i].nombreFiesta;
+                            
+                            imagen2.Source = ultimasFiestas[i].imagen;
+                            break;
+                        case 2:
+                            Fiesta3.Text = ultimasFiestas[i].nombreFiesta;
+                           
+                            imagen3.Source = ultimasFiestas[i].imagen;
+                            break;
+                    }
+                }
+
+                labelDatos.Text = "Fiestas actualizadas correctamente";
+            }
+            catch (Exception ex)
+            {
+                labelDatos.Text = $"Error al actualizar las fiestas: {ex.Message}";
+            }
+
+        }
+
+        private void Reservar_Clicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync(nameof(ReservaPage));
         }
     }
 
